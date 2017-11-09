5e1813e03948123a1bf8d137d5f27ffde1779238
knpt33 <knpt33@mail.missouri.edu>
2017-11-09 16:00:40 -0600

    kjjjj

--- menu1.js
+++ menu1.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $('.menu-trigger').click(function(){
-    $('nav ul').slideToggle(500);
+    $('nav ul').slideToggle(400);
   });
   $(window).resize(function(){
-    if($(window).width()>500){
+    if($(window).width()>400){
       $('nav ul').removeAttri('style');
     }
   });
